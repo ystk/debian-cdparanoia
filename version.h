0eaa5f524c1d51a502ef7870a4427b648cfdf495
KOBAYASHI Yoshitake <yoshi@sbuild1.swc.toshiba.co.jp>
2008-09-16 14:01:47 +0900

    git-debimport cdparanoia_3.10.2+debian.orig.tar.gz

---
+++ version.h
@@ -0,0 +1,11 @@
+/******************************************************************
+ * CopyPolicy: GNU Public License 2 applies
+ * 
+ * cdda_paranoia generation III release 10.2
+ * Copyright (C) 2008 Monty monty@xiph.org
+ *
+ ******************************************************************/
+
+
+#define VERSIONNUM "10.2"
+#define VERSION "cdparanoia III release " VERSIONNUM " (September 11, 2008)\n"
